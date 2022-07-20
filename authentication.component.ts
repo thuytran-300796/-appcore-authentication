8f46ccce9488d2b809e99f1dfaf3f8d70ce78f7f
Jadie <56433200+thuytran-300796@users.noreply.github.com>
2022-07-20 15:45:30 +0700

    Add files via upload
    update authentication.component

---
+++ authentication.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'appcore-authentication',
+  template: `
+    <p>
+      authentication works! (new)
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class AuthenticationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
